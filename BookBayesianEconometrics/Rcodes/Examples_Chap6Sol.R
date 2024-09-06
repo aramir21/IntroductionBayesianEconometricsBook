95c1b7f5d7b6bb447070ec12990e152983a79e63
aramir21 <aramir21@eafit.edu.co>
2024-09-06 09:57:55 -0500

    Working in chapter 6 V1

---
+++ BookBayesianEconometrics/Rcodes/Examples_Chap6Sol.R
@@ -0,0 +1,66 @@
+rm(list = ls())
+set.seed(010101)
+########################## Linear regression: Value of soccer players ##########################
+Data <- read.csv("DataApplications/1ValueFootballPlayers.csv", sep = ",", header = TRUE, fileEncoding = "latin1")
+attach(Data)
+y <- log(Value) 
+# Value: Market value in Euros (2017) of soccer players
+# Regressors quantity including intercept
+X <- cbind(1, Perf, Age, Age2, NatTeam, Goals, Exp, Exp2)
+# Perf: Performance. Perf2: Performance squared. Age: Age; Age: Age squared. 
+# NatTeam: Indicator of national team. Goals: Scored goals. Goals2: Scored goals squared
+# Exp: Years of experience. Exp2: Years of experience squared. Assists: Number of assists
+k <- dim(X)[2]
+N <- dim(X)[1]
+# Hyperparameters
+d0 <- 0.001/2
+a0 <- 0.001/2
+b0 <- rep(0, k)
+c0 <- 1000
+B0 <- c0*diag(k)
+B0i <- solve(B0)
+v <- 5
+# MCMC parameters
+mcmc <- 5000
+burnin <- 5000
+tot <- mcmc + burnin
+thin <- 1
+# Posterior distributions programming the Gibbs sampling
+# Auxiliary parameters
+an <- a0 + N
+v1n <- v + 1
+# Gibbs sampling functions
+PostSig2 <- function(Beta, tau){
+  dn <- d0 + t(y - X%*%Beta)%*%diag(tau)%*%(y - X%*%Beta)
+  sig2 <- invgamma::rinvgamma(1, shape = an/2, rate = dn/2)
+  return(sig2)
+}
+PostBeta <- function(sig2, tau){
+  Bn <- solve(B0i + sig2^(-1)*t(X)%*%diag(tau)%*%X)
+  bn <- Bn%*%(B0i%*%b0 + sig2^(-1)*t(X)%*%diag(tau)%*%y)
+  Beta <- MASS::mvrnorm(1, bn, Bn)
+  return(Beta)
+}
+PostTau <- function(sig2, Beta, i){
+  v2n <- v + sig2^(-1)*(y[i]-X[i,]%*%Beta)^2
+  taui <- rgamma(1, v1n, v2n)
+  return(taui)
+}
+PostBetas <- matrix(0, mcmc+burnin, k)
+PostSigma2 <- rep(0, mcmc+burnin)
+Beta <- rep(0, k)
+tau <- rep(1, N)
+for(s in 1:tot){
+  sig2 <- PostSig2(Beta = Beta, tau = tau)
+  PostSigma2[s] <- sig2
+  Beta <- PostBeta(sig2 = sig2, tau = tau)
+  PostBetas[s,] <- Beta
+  tau <- sapply(1:N, function(i){PostTau(sig2 = sig2, Beta = Beta, i)})
+}
+keep <- seq((burnin+1), tot, thin)
+PosteriorBetas <- PostBetas[keep,]
+colnames(PosteriorBetas) <- c("Intercept", "Perf", "Age", "Age2", "NatTeam", "Goals", "Exp", "Exp2")
+summary(coda::mcmc(PosteriorBetas))
+PosteriorSigma2 <- PostSigma2[keep]
+summary(coda::mcmc(PosteriorSigma2))
+summary(coda::mcmc(exp(PosteriorBetas[,5])-1))
