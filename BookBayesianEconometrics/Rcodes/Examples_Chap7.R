ea95e81b0e698b992d8da95a437dfd9a20860363
aramir21 <aramir21@eafit.edu.co>
2024-09-13 04:34:37 -0500

    Multivariate simple model V1

---
+++ BookBayesianEconometrics/Rcodes/Examples_Chap7.R
@@ -0,0 +1,36 @@
+########################## Effects of institutions: Multivariate regression ########################## 
+rm(list = ls())
+set.seed(010101)
+DataInst <- read.csv("DataApplications/4Institutions.csv", sep = ",", header = TRUE, fileEncoding = "latin1")
+attach(DataInst)
+Y <- cbind(logpcGDP95, PAER)
+X <- cbind(1, logMort, Africa, Asia, Other)
+M <- dim(Y)[2]
+K <- dim(X)[2]
+N <- dim(Y)[1]
+# Hyperparameters
+B0 <- matrix(0, K, M)
+c0 <- 100
+V0 <- c0*diag(K)
+Psi0 <- 5*diag(M)
+a0 <- 5
+# Posterior parameters
+Bhat <- solve(t(X)%*%X)%*%t(X)%*%Y 
+S <- t(Y - X%*%Bhat)%*%(Y - X%*%Bhat)
+Vn <- solve(solve(V0) + t(X)%*%X) 
+Bn <- Vn%*%(solve(V0)%*%B0 + t(X)%*%X%*%Bhat)
+Psin <- Psi0 + S + t(B0)%*%solve(V0)%*%B0 + t(Bhat)%*%t(X)%*%X%*%Bhat - t(Bn)%*%solve(Vn)%*%Bn
+an <- a0 + N
+#Posterior draws
+s <- 10000 #Number of posterior draws
+SIGs <- replicate(s, LaplacesDemon::rinvwishart(an, Psin))
+BsCond <- sapply(1:s, function(s) {MixMatrix::rmatrixnorm(n = 1, mean=Bn, U = Vn,V = SIGs[,,s])})
+summary(coda::mcmc(t(BsCond)))
+SIGMs <- t(sapply(1:s, function(l) {gdata::lowerTriangle(SIGs[,,l], diag=TRUE, byrow=FALSE)}))
+summary(coda::mcmc(SIGMs))
+hdiBs <- HDInterval::hdi(t(BsCond), credMass = 0.95) # Highest posterior density credible interval
+hdiBs
+hdiSIG <- HDInterval::hdi(SIGMs, credMass = 0.95) # Highest posterior density credible interval
+hdiSIG
+beta1 <- BsCond[2,]/BsCond[7,] 
+summary(coda::mcmc(beta1)) # Effect of property rights on GDP
