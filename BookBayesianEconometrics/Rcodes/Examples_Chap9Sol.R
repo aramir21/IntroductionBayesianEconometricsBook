dd2d9d1ec10004a09cdfaef32feff809a5a8e55a
aramir21 <aramir21@eafit.edu.co>
2024-10-02 04:42:25 -0500

    Hierarchical normal V9

--- BookBayesianEconometrics/Rcodes/Examples_Chap9Sol.R
+++ BookBayesianEconometrics/Rcodes/Examples_Chap9Sol.R
@@ -317,10 +317,10 @@ for(i in 1:NT){
   y <- c(y, yi)
 }
 Data <- as.data.frame(cbind(y, x1, x2, x3, zdata, w1, id))
-mcmc <- 5000; burnin <- 1000; thin <- 1; tot <- mcmc + burnin
+mcmc <- 15000; burnin <- 5000; thin <- 10; tot <- mcmc + burnin
 b0 <- rep(0, K1); B0 <- diag(K1); B0i <- solve(B0) 
 r0 <- K2; R0 <- diag(K2); a0 <- 0.001; d0 <- 0.001
-g0 <- rep(0, K2); G0 <- diag(K2)
+g0 <- rep(0, K2*K3); G0 <- diag(K2*K3); G0i <- solve(G0)
 # MCMChregress
 Resultshreg <- MCMCpack::MCMChregress(fixed = y~x1 + x2 + x3 + zdata, random = ~w1, group="id",
                                       data = Data, burnin = burnin, mcmc = mcmc, thin = thin, 
@@ -336,7 +336,7 @@ coda::geweke.diag(Betas)
 coda::raftery.diag(Betas,q=0.5, r=0.05, s = 0.95)
 coda::heidel.diag(Betas)
 # Gibbs by hand
-PostBeta <- function(sig2, D){
+PostBeta <- function(sig2, Gamma, D){
   XVX <- matrix(0, K1, K1)
   XVy <- matrix(0, K1, 1)
   for(i in 1:N){
@@ -348,8 +348,10 @@ PostBeta <- function(sig2, D){
     Xi <- X[ids, ]
     XVXi <- t(Xi)%*%ViInv%*%Xi
     XVX <- XVX + XVXi
+    Zi <- Z[i, ]
     yi <- y[ids]
-    XVyi <- t(Xi)%*%ViInv%*%yi
+    ZGi <- t(kronecker(diag(K2), Zi))%*%Gamma
+    XVyi <- t(Xi)%*%ViInv%*%(yi - Wi%*%ZGi)
     XVy <- XVy + XVyi
   }
   Bn <- solve(B0i + XVX)
@@ -357,30 +359,9 @@ PostBeta <- function(sig2, D){
   Beta <- MASS::mvrnorm(1, bn, Bn)
   return(Beta)
 }
-# This version of the function follows MCMCpack function named cMCMChregress.cc that uses 
-# http://en.wikipedia.org/wiki/Woodbury_matrix_identity with A=(1/V_run)*Id
-PostBetaNew <- function(sig2, D){
-  Di <- solve(D)
-  XVXn <- matrix(0, K1, K1)
-  XVyn <- matrix(0, K1, 1)
-  for(i in 1:N){
-    ids <- which(id == i)
-    Ti <- length(ids)
-    Wi <- W[ids, ]
-    Xi <- X[ids, ]
-    yi <- y[ids]
-    sig2i <- sig2^(-1)
-    XVXi <- t(Xi)%*%Xi*sig2i - sig2i^(2)*t(Xi)%*%Wi%*%solve(Di+sig2i*t(Wi)%*%Wi)%*%t(Wi)%*%Xi
-    XVyi <- t(Xi)%*%yi*sig2i - sig2i^(2)*t(Xi)%*%Wi%*%solve(Di+sig2i*t(Wi)%*%Wi)%*%t(Wi)%*%yi 
-    XVXn <- XVXn + XVXi
-    XVyn <- XVyn + XVyi
-  }
-  BnNew <- solve(B0i + XVXn)
-  bnNew <- BnNew%*%(B0i%*%b0 + XVyn)
-  Beta <- MASS::mvrnorm(1, bnNew, BnNew)
-  return(Beta)
-}
-Postb <- function(Beta, sig2, D){
+# sig2 <- sig2; D <- matrix(c(0.7,0,0,0.6),2,2); Gamma <- G
+# rowMeans(replicate(100, PostBeta(sig2, Gamma, D)))
+Postb <- function(Beta, sig2, Gamma, D){
   Di <- solve(D)
   bis <- matrix(0, N, K2)
   for(i in 1:N){
@@ -390,12 +371,16 @@ Postb <- function(Beta, sig2, D){
     yi <- y[ids]
     Wtei <- sig2^(-1)*t(Wi)%*%(yi - Xi%*%Beta)
     Bni <- solve(sig2^(-1)*t(Wi)%*%Wi + Di)
-    bni <- Bni%*%Wtei
+    Zi <- Z[i, ]
+    ZGi <- t(kronecker(diag(K2), Zi))%*%Gamma
+    bni <- Bni%*%(Wtei + Di%*%ZGi)
     bi <- MASS::mvrnorm(1, bni, Bni)
     bis[i, ] <- bi
   }
   return(bis)
 }
+# sig2 <- sig2; D <- matrix(c(0.7,0,0,0.6),2,2); Gamma <- G; Beta <- B
+# Ens <- replicate(100, rowMeans(Postb(Beta, sig2, Gamma, D) - b))
 PostSig2 <- function(Beta, bs){
   an <- a0 + 0.5*NT
   ete <- 0
@@ -412,19 +397,46 @@ PostSig2 <- function(Beta, bs){
   sig2 <- MCMCpack::rinvgamma(1, shape = an, scale = dn)
   return(sig2)
 }
-PostD <- function(bs){
+# Beta <- B; bs <- b
+# mean(replicate(100, PostSig2(Beta, bs)))
+PostGamma <- function(bs, D){
+  Di <- solve(D)
+  ZDZ <- matrix(0, K2*K3, K2*K3)
+  ZDb <- matrix(0, K2*K3, 1)
+  for(i in 1:N){
+    Zi <- Z[i, ]
+    ZZi <- t(kronecker(diag(K2), Zi))
+    ZDZi <- t(ZZi)%*%Di%*%ZZi
+    ZDZ <- ZDZ + ZDZi
+    bi <- bs[i,]
+    ZDbi <- t(ZZi)%*%Di%*%bi
+    ZDb <- ZDb + ZDbi
+  }
+  Gn <- solve(G0i + ZDZ)
+  gn <- Gn%*%(G0i%*%g0 + ZDb)
+  Gamma <- MASS::mvrnorm(1, gn, Gn)
+  return(Gamma)
+}
+# bs <- b; D <- matrix(c(0.7,0,0,0.6),2,2)
+# rowMeans(replicate(100, PostGamma(bs, D)))
+PostD <- function(bs, Gamma){
   rn <- r0 + N
   btb <- matrix(0, K2, K2)
   for(i in 1:N){
     bsi <- bs[i, ]
-    btbi <- bsi%*%t(bsi)
+    Zi <- Z[i, ]
+    ZGi <- t(kronecker(diag(K2), Zi))%*%Gamma
+    btbi <- (bsi-ZGi)%*%t(bsi-ZGi)
     btb <- btb + btbi
   }
   Rn <- d0*R0 + btb
   Sigma <- MCMCpack::riwish(v = rn, S = Rn)
   return(Sigma)
 }
+# bs <- b; Gamma <- G
+# rowMeans(replicate(100, PostD(bs, gamma)[c(1,4)]))
 PostBetas <- matrix(0, tot, K1)
+PostGammas <- matrix(0, tot, K2*K3)
 PostDs <- matrix(0, tot, K2*(K2+1)/2)
 PostSig2s <- rep(0, tot)
 Postbs <- array(0, c(N, K2, tot))
@@ -432,18 +444,20 @@ RegLS <- lm(y ~ X - 1)
 SumLS <- summary(RegLS)
 Beta <- SumLS[["coefficients"]][,1]
 sig2 <- SumLS[["sigma"]]^2
+Gamma <- rep(0, K2*K3)
 D <- diag(K2)
 pb <- winProgressBar(title = "progress bar", min = 0, max = tot, width = 300)
 for(s in 1:tot){
-  bs <- Postb(Beta = Beta, sig2 = sig2, D = D)
-  D <- PostD(bs = bs)
-  Beta <- PostBeta(sig2 = sig2, D = D)
-  # Beta <- PostBetaNew(sig2 = sig2, D = D)
+  bs <- Postb(Beta = Beta, sig2 = sig2, Gamma = Gamma, D = D)
+  D <- PostD(bs = bs, Gamma = Gamma)
+  Beta <- PostBeta(sig2 = sig2, Gamma = Gamma, D = D)
   sig2 <- PostSig2(Beta = Beta, bs = bs)
+  Gamma <- PostGamma(bs = bs, D = D) 
   PostBetas[s,] <- Beta
   PostDs[s,] <- matrixcalc::vech(D)
   PostSig2s[s] <- sig2
   Postbs[, , s] <- bs
+  PostGammas[s,] <- Gamma
   setWinProgressBar(pb, s, title=paste( round(s/tot*100, 0),"% done"))
 }
 close(pb)
@@ -452,12 +466,15 @@ Bs <- PostBetas[keep,]
 Ds <- PostDs[keep,]
 bs <- Postbs[, , keep]
 sig2s <- PostSig2s[keep]
+Gs <- PostGammas[keep, ]
 summary(coda::mcmc(Bs))
 plot(coda::mcmc(Bs))
 summary(coda::mcmc(Ds))
 plot(coda::mcmc(Ds))
 summary(coda::mcmc(sig2s))
 plot(coda::mcmc(sig2s))
+summary(coda::mcmc(Gs))
+plot(coda::mcmc(Gs))
 # Convergence diagnostics
 coda::geweke.diag(Bs)
 coda::raftery.diag(Bs, q = 0.5, r = 0.05, s = 0.95)
