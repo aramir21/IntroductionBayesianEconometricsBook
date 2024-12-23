e5327d699ad198da3d3b6ba5bfb5fd7a3467c4b0
aramir21 <aramir21@eafit.edu.co>
2024-12-23 07:13:16 -0500

    Simulation methods V19

--- BookBayesianEconometrics/Rcodes/Examples_Chap5.R
+++ BookBayesianEconometrics/Rcodes/Examples_Chap5.R
@@ -34,10 +34,10 @@ for(s in 1:S){
 close(pb)
 library(coda)
 theta1Post <- mcmc(theta1[keep])
-plot(theta1Post); summary(theta1Post); autocorr.plot(theta1Post)
+summary(theta1Post)
 theta2Post <- mcmc(theta2[keep])
-plot(theta2Post); summary(theta2Post); autocorr.plot(theta2Post)
-HPost <- mcmc(kk); plot(HPost); summary(HPost); autocorr.plot(HPost)
+summary(theta2Post)
+HPost <- mcmc(kk); summary(HPost)
 hist(HPost, main = "Histogram: Posterior mean change point", xlab = "Posterior mean", col = "blue", breaks = 25)
 
 ########################## Metropolis-Hastings: Beta posterior########################## 
@@ -339,4 +339,50 @@ plot_filtering_estimates(df)
 MargLik <- colMeans(weights)
 plot(MargLik, type = "l")
 matplot(1:T, t(weights), type = "l", lty = 1, col = rgb(0, 0, 0, 0.1), main = "Particle Weights Over Time", ylab = "Weight", xlab = "Time")
+
+########################## Mining change point ########################## 
+rm(list = ls())
+set.seed(010101)
+dataset<-read.csv("https://raw.githubusercontent.com/besmarter/BSTApp/refs/heads/master/DataApp/MiningDataCarlin.csv",header=T)
+attach(dataset)
+str(dataset)
+a10 <- 0.5; a20 <- 0.5
+b10 <- 1; b20 <- 1
+y <- Count
+sumy <- sum(Count); T <- length(Count)
+theta1 <- NULL; theta2 <- NULL
+kk <- NULL; H <- 60
+MCMC <- 20000; burnin <- 1000; S <- MCMC + burnin; keep <- (burnin+1):S
+pb <- winProgressBar(title = "progress bar", min = 0, max = S, width = 300)
+for(s in 1:S){
+  a1 <- a10 + sum(y[1:H])
+  b1 <- b10+H
+  theta11 <- rgamma(1,a1,b1)
+  theta1 <- c(theta1,theta11)
+  a2 <- a20 + sum(y[(1+H):T])
+  b2 <- b20 + T-H
+  theta22 <- rgamma(1,a2,b2)
+  theta2 <- c(theta2,theta22)
+  pp<-NULL
+  for(l in 1:T){
+    p <- exp(l*(theta22-theta11))*(theta11/theta22)^(sum(y[1:l]))
+    pp <- c(pp,p)
+  }
+  prob <- pp/sum(pp)
+  H <- sample(1:T,1,prob=prob)
+  kk <- c(kk,H)
+  setWinProgressBar(pb, s, title=paste( round(s/S*100, 0),"% done"))
+}
+close(pb)
+library(coda); library(latex2exp)
+theta1Post <- mcmc(theta1[keep]); summary(theta1Post)
+HPost <- mcmc(kk); summary(HPost)
+theta2Post <- mcmc(theta2[keep])
+plot(theta2Post, density = FALSE, main = "Trace plot", ylab = TeX("$\\lambda_{2}$"))
+autocorr.plot(theta2Post, main = "Autocorrelation plot")
+summary(theta2Post); effectiveSize(theta2Post) 
+geweke.diag(theta2Post, frac1 = 0.1, frac2 = 0.5)
+raftery.diag(theta2Post, q = 0.025, r = 0.005, s = 0.95)
+heidel.diag(theta2Post, eps = 0.1, pvalue = 0.05)
+
 
