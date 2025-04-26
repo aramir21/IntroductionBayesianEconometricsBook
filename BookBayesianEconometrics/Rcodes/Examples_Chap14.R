2d1075b2f6bfd6e82908ab44dee5bcc96f6b0e5e
aramir21 <aramir21@eafit.edu.co>
2025-04-26 05:55:46 -0500

    Chapter 14 V18

--- BookBayesianEconometrics/Rcodes/Examples_Chap14.R
+++ BookBayesianEconometrics/Rcodes/Examples_Chap14.R
@@ -323,6 +323,109 @@ legend("topright", legend = c("Simplified Laplace", "Gaussian", "Full Laplace", 
 # Summary variance sla
 inla.qmarginal(c(0.025, 0.5, 0.975), marg.var.sla) # 95% credible interval
 
+#################### INLA: Spatial econometrics #####################
+rm(list = ls()); set.seed(010101)
+# setwd('C:/Users/aramir21/Desktop/BookBayesianInference/IntroductionBayesianEconometricsBook/BookBayesianEconometrics/Rcodes/DataApplications/Antioquia')
+library(sf)
+library(tmap)
+library(classInt)
+library(RColorBrewer)
+library(spdep)
+library(parallel)
+library(INLABMA)
+library(sf)
+zip_url <- "https://github.com/BEsmarter-consultancy/BSTApp/archive/refs/heads/master.zip"
+temp_zip <- tempfile(fileext = ".zip")
+download.file(zip_url, temp_zip, mode = "wb")
+# Extract to a temp folder
+temp_dir <- tempdir()
+unzip(temp_zip, exdir = temp_dir)
+# Path to the folder
+antioquia_path <- file.path(temp_dir, "BSTApp-master", "DataApp", "Antioquia")
+list.files(antioquia_path)  # Check contents
+# Print the full path to the "Antioquia" folder
+antioquia_path <- file.path(temp_dir, "BSTApp-master", "DataApp", "Antioquia")
+print(antioquia_path)
+# Path to the shapefile
+shp_file <- file.path(antioquia_path, "Antioquia.shp")
+# Read the shapefile
+antioquia <- st_read(shp_file)
+# Check the structure of the shapefile
+print(antioquia)
+# View the result
+plot(st_geometry(antioquia), main = "Map of Antioquia")
+# Replace with your actual shapefile name
+antioquia <- st_read("antioquia.shp")
+# Create class intervals and factor
+interval <- classIntervals(antioquia$CONS_OLD, 5, style = "quantile")
+antioquia$cons_class <- cut(
+  antioquia$CONS_OLD,
+  breaks = interval$brks,
+  include.lowest = TRUE
+)
+# Red palette
+plotcolors <- brewer.pal(5, "Reds")
+# Set mode
+tmap_mode("plot")
+# tmap v4 syntax
+# Per capita annual average electricity consumption: Stratum 1, Antioquia (Colombia)
+tm_shape(antioquia) +
+  tm_fill(
+    fill = "cons_class",
+    fill.scale = tm_scale(values = plotcolors),
+    fill.legend = tm_legend(title = "Electricity consumption")
+  ) +
+  tm_borders(col = "grey90") +
+  tm_layout(legend.outside = TRUE)
+
+# Create neighbors list using queen contiguity (default)
+nb_object <- poly2nb(antioquia, queen = TRUE)
+# Get centroids as coordinates matrix
+centroids <- st_centroid(st_geometry(antioquia))
+coords <- st_coordinates(centroids)
+# Plot the map and neighbor links
+# windows(width = 10, height = 8)  # size in inches
+plot(st_geometry(antioquia), border = "grey")
+plot(nb_object, coords, add = TRUE, col = "red")
+# title(main = "Contiguity map: Queen criterion, municipalities (Antioquia, Colombia)")
+attach(antioquia)
+fform <- L_CONS_OLD ~ L_P_OLD + L_ING_US + L_P_SUST 
+RegOLS <- lm(fform)
+summary(RegOLS)
+res <- RegOLS$residuals
+NBList <- nb2listw(nb_object, style = "B")
+moran_mc<- moran.mc(res, listw = NBList, 10000)
+moran_mc
+LM<-localmoran(as.vector(res), NBList)
+sum(LM[,5]<0.05)
+
+zero.variance <- list(prec = list(initial = 25, fixed = TRUE))
+ant.mat <- nb2mat(nb_object)
+bmsp <- as(ant.mat, "CsparseMatrix")
+antioquia$idx <- 1:nrow(antioquia)
+rrho1 <- seq(0.5, 0.95, len = 25)
+semmodels <- mclapply(rrho1, function(rho) {
+  sem.inla(fform, d = as.data.frame(antioquia), W = bmsp, rho = rho,
+             family = "gaussian", impacts = FALSE,
+             control.family = list(hyper = zero.variance),
+             control.predictor = list(compute = TRUE),
+             control.compute = list(dic = TRUE, cpo = TRUE),
+             control.inla = list(print.joint.hyper = TRUE))
+})
+bmasem <- INLABMA(semmodels, rrho1, 0, impacts = FALSE)
+#Display results
+plot(bmasem$rho$marginal, type="l", col = "blue", lwd = 2,
+     xlab = expression(beta[x]), ylab = "Density",
+     main = "Spatial error model: Posterior price elasticity")
+
+bmasem[["rho"]][["quantiles"]]
+
+marg_sla <- bmasem[["marginals.fixed"]][["L_P_OLD"]]
+plot(marg_sla, type = "l", col = "blue", lwd = 2,
+     xlab = expression(beta[x]), ylab = "Density",
+     main = "Spatial error model: Posterior price elasticity")
+bmasem[["summary.fixed"]]
+
 ######### Variational Bayes: Linear regression ##########
 set.seed(010101)
 library(LaplacesDemon)
